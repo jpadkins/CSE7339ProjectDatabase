ee49c6b599fcb0652ce92f3b53f174b1c3db983b
jacobpadkins <jpadkins@smu.edu>
2016-12-04 14:23:04 -0600

    updated readme

--- cloud_storage_app_database.py
+++ cloud_storage_app_database.py
@@ -1,4 +1,6 @@
 import sqlite3
+import crypt
+import hashlib
 from enum import Enum
 
 class API(Enum):
@@ -9,10 +11,10 @@ class CloudStorageAppDatabase(object):
         self.__conn=sqlite3.connect(db_file)
         stmt = \
                 """ CREATE TABLE IF NOT EXISTS users
-                (id INTEGER, username TEXT, password TEXT, salt TEXT,
-                drive_token TEXT, drive_timestamp TEXT, dropbox_token TEXT,
-                dropbox_timestamp TEXT, box_token TEXT, box_timestamp TEXT
-                ) """
+                (username TEXT NOT NULL, password_hash TEXT NOT NULL,
+                salt TEXT NOT NULL, drive_token TEXT, drive_timestamp TIMESTAMP,
+                dropbox_token TEXT, dropbox_timestamp TIMESTAMP,
+                box_token TEXT, box_timestamp TIMESTAMP) """
         self.__conn.cursor().execute(stmt)
         self.__conn.commit()
 
@@ -20,19 +22,30 @@ class CloudStorageAppDatabase(object):
         self.__conn.close()
 
     # add a user to the database
-    def add_user(username, password):
-        try:
-            self.__conn.cursor().execute(stmt)
+    def add_user(self, username, password):
+        key = (username,)
+        stmt = "SELECT EXISTS (SELECT 1 FROM users WHERE username=? LIMIT 1)"
+        c = self.__conn.cursor().execute(stmt, key)
+        if not c.fetchone():
+            salt = crypt.mksalt(crypt.METHOD_SHA512)
+            password_hash = hashlib.sha512(salt+password).hexdigest()
+            stmt = \
+                   """ INSERT INTO users (username, password_hash, salt,
+                   drive_token, drive_timestamp, dropbox_token,
+                   dropbox_timestamp, box_token, box_timestamp) VALUES
+                   (?,?,?,?,?,?,?,?,?) """
+            self.__conn.cursor().execute(stmt, username, password_hash, salt,
+                                         None, None, None, None, None, None)
+            self.__conn.commit()
 
     # get a user id from the database
-    def get_user_id(username, password):
-        try:
-            ...
+    def get_user_id(self, username, password):
+        return True
 
     # set the auth token in the database
-    def set_auth_token(user_id, api, token):
-        ...
+    def set_auth_token(self, user_id, api, token):
+        return True
 
     # get an auth token from the database
-    def get_auth_token(user_id, api):
-        ...
+    def get_auth_token(self, user_id, api):
+        return True
